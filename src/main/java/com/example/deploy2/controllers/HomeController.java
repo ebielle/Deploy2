3a2cada592ba43616a336529f89b465ffa358ce2
ebielle <eryblongo@gmail.com>
2024-08-02 21:56:54 +0200

    finished

--- src/main/java/com/example/deploy2/controllers/HomeController.java
+++ src/main/java/com/example/deploy2/controllers/HomeController.java
@@ -1,14 +1,33 @@
 package com.example.deploy2.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-    @GetMapping("/name")
-    public String helloName(){
-        String devName = "Erika";
-        return devName;
+    @Autowired
+    private Environment environment;
+
+    @Value("${myDevNameTree.devName}")
+    String myDevName;
+
+    @GetMapping("/hello")
+    public String getHello(){
+        return "Hello!";
+    }
+
+    @GetMapping("/")
+    public String mainEndpoint(){
+        String var = environment.getProperty("myDevNameTree.devName");
+        return var;
+    }
+
+    @GetMapping("/other-injection")
+    public String mainEndpointII(){
+        return myDevName;
     }
 }
